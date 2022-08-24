c02624b23c833635d2191d4ca7831f26f89f7b1c
evreddy <vinayreddy.eedunuri@gmail.com>
2022-08-24 20:48:09 +0530

    Update sampleapi.py
    syntax errors rectified

--- sampleapi.py
+++ sampleapi.py
@@ -1,6 +1,7 @@
-import requests, json
+import requests
+import json
 
 base_url = 'https://api.cisco.com/product/v1/information/serial_numbers/SPE181700LN,REF_CSJ07306405'
 response = requests.get(base_url)
-data = response.jason()
+data = response.json()
 print(data)
