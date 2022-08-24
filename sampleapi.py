671745e277df70f9076c83d88eebf18d3f7deb87
evreddy <vinayreddy.eedunuri@gmail.com>
2022-08-24 20:42:01 +0530

    Update sampleapi.py

--- sampleapi.py
+++ sampleapi.py
@@ -1,6 +1,6 @@
-import requests
+import requests, json
 
 base_url = 'https://api.cisco.com/product/v1/information/serial_numbers/SPE181700LN,REF_CSJ07306405'
-rep = requests.get(base_url)
+response = requests.get(base_url)
 data = response.jason()
 print(data)
