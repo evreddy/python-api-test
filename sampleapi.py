09d517afef75971ffc247356e83c0aa06feb6638
evreddy <vinayreddy.eedunuri@gmail.com>
2022-08-25 18:56:04 +0530

    syntax is corrected

--- sampleapi.py
+++ sampleapi.py
@@ -1,9 +1,8 @@
-from _future_ import print_function
 import requests, json
 #declaring the url
 url = 'https://api.cisco.com/product/v1/information/serial_numbers/SPE181700LN'
 
-headers = { 'Accept': 'application\json', 'Content_Type': 'application\json' }
+headers = { 'Accept': 'application/json', 'Content_Type': 'application/json' }
 
 response = requests.request("GET", url, headers=headers, data={})
 data = response.json()
