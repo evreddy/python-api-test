b612351f55be8deb690bf6a9b99b1fd54cf0a231
evreddy <vinayreddy.eedunuri@gmail.com>
2022-08-25 19:05:09 +0530

    syntax is corrected

--- sampleapi.py
+++ sampleapi.py
@@ -4,6 +4,6 @@ url = 'https://api.cisco.com/product/v1/information/serial_numbers/SPE181700LN'
 
 headers = { 'Accept': 'application/json', 'Content_Type': 'application/json' }
 
-response = requests.request("GET", url, headers=headers, serial_numbers={})
-serial_numbers = response.json()
-print(serial_numbers)
+response = requests.request("GET", url=url, headers=headers, srno={})
+srno = response.json()
+print(srno)
