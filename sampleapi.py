7e25411c5bdf0a64441e9860e102104ff3bab6cf
evreddy <vinayreddy.eedunuri@gmail.com>
2022-08-25 18:52:30 +0530

    Update sampleapi.py
    modified

--- sampleapi.py
+++ sampleapi.py
@@ -3,10 +3,7 @@ import requests, json
 #declaring the url
 url = 'https://api.cisco.com/product/v1/information/serial_numbers/SPE181700LN'
 
-headers = { 
-        'Accept': 'application\json'
-        'Content-Type': 'application\json'
-}
+headers = { 'Accept': 'application\json', 'Content_Type': 'application\json' }
 
 response = requests.request("GET", url, headers=headers, data={})
 data = response.json()
